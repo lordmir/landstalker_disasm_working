39dbaea850437dece6c88ff0ecd52d266a69377c
hase <hase@redfern.xyz>
2023-03-16 18:37:47 +0000

    Add region check code

---
+++ disassembly/code/system/regioncheck.asm
@@ -0,0 +1,4 @@
+                            include "code\system\regioncheck_routine.asm"
+                            include "code\system\regioncheck_strings.asm"
+                            Align   $2
+SystemFont:                 incbin  "assets_packed\graphics\fonts\system.bin"
