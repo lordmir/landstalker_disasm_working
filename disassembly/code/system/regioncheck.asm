1ff73def57ae41fcdaed5bc2089ba97b70efa0f6
hase <hase@redfern.xyz>
2023-03-16 20:45:51 +0000

    Fixes to region check code

--- disassembly/code/system/regioncheck.asm
+++ disassembly/code/system/regioncheck.asm
@@ -1,4 +1,4 @@
-                            include "code\system\regioncheck_routine.asm"
-                            include "code\system\regioncheck_strings.asm"
+RegionCheckRoutine:         include "code\system\regioncheck_routine.asm"
+RegionCheckStrings:         include "code\system\regioncheck_strings.asm"
                             Align   $2
 SystemFont:                 incbin  "assets_packed\graphics\fonts\system.bin"
--- disassembly/code/system/regioncheck_strings.asm
+++ disassembly/code/system/regioncheck_strings.asm
@@ -1,5 +1,12 @@
-RegionErrorLine1:	dc.b '  DEVELOPED FOR USE ONLY WITH',0 ; DATA XREF: CheckRegion+1Et
-RegionErrorNTSC:	dc.b '         NTSC GENESIS',0    ; DATA XREF: CheckRegion+36t
-RegionErrorPAL:		dc.b 'PAL AND FRENCH SECAM MEGA DRIVE',0
-						  ; DATA XREF: CheckRegion:loc_11EAAAt
-RegionErrorLine3:	dc.b '           SYSTEMS.',0      ; DATA XREF: CheckRegion:loc_11EAB8t
+RegionErrorLine1:dc.b $20, $20, $44, $45, $56, $45, $4C, $4F, $50, $45, $44, $20, $46, $4F, $52, $20
+                                                  ; DATA XREF: CheckRegion+1Et
+                dc.b $55, $53, $45, $20, $4F, $4E, $4C, $59, $20, $57, $49, $54, $48, $00
+RegionErrorNTSC:dc.b $20, $20, $20, $20, $20, $20, $20, $20, $20, $4E, $54, $53, $43, $20, $47, $45
+                                                  ; DATA XREF: CheckRegion+36t
+                dc.b $4E, $45, $53, $49, $53, $00
+RegionErrorPAL: dc.b $50, $41, $4C, $20, $41, $4E, $44, $20, $46, $52, $45, $4E, $43, $48, $20, $53
+                                                  ; DATA XREF: CheckRegion:loc_11EAAAt
+                dc.b $45, $43, $41, $4D, $20, $4D, $45, $47, $41, $20, $44, $52, $49, $56, $45, $00
+RegionErrorLine3:dc.b $20, $20, $20, $20, $20, $20, $20, $20, $20, $20, $20, $53, $59, $53, $54, $45
+                                                  ; DATA XREF: CheckRegion:loc_11EAB8t
+                dc.b $4D, $53, $2E, $00
